eebe84adc1b7d21c9d15de12626a16d94a9e020d
Toni Hermoso Pulido <toniher@cau.cat>
2020-06-22 16:14:13 +0200

    minor fix

--- wikibase-start-wiki.sh
+++ wikibase-start-wiki.sh
@@ -13,7 +13,7 @@ if [ ! $MW_NEW ]; then
 fi
 
 docker run --net=$NETWORK -p $PORT:80 -v ${MW_IMAGES}:/var/www/w/images \
-${LOCALSETTINGS_MOUNT} -v ${CONF_PATH}/LocalSettings.local.php:/var/www/w/LocalSettings.local.php -v ${CONF_PATH}/LocalSettings.redis.php:/var/www/w/LocalSettings.redis.php -v ${CONF_PATH}/LocalSettings.elastica.php:/var/www/w/LocalSettings.elastica.php \ 
+${LOCALSETTINGS_MOUNT} -v ${CONF_PATH}/LocalSettings.local.php:/var/www/w/LocalSettings.local.php -v ${CONF_PATH}/LocalSettings.redis.php:/var/www/w/LocalSettings.redis.php -v ${CONF_PATH}/LocalSettings.elastica.php:/var/www/w/LocalSettings.elastica.php \
 --name $WIKI_CONTAINER --network-alias=$DOMAIN_NAME -d $WIKI_IMAGE
 
 echo "Running parsoid"
